237f35a2cbb0722c9e99e03604f9197086c0c572
Sebastian Faliani <nedase@gmail.com>
2022-11-16 09:41:46 -0300

    Agrego Proyecto Calculadora

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+let sumar = require("./sumar");
+let restar = require("./restar");
+let multiplicar = require("./multiplicar");
+let dividir = require("./dividir");
+
+console.log(sumar(2, 3));
+console.log(restar(4, 5));
+console.log(multiplicar(6, 7));
+console.log(multiplicar(8, 0));
+console.log(dividir(9, 2));
+console.log(dividir(2, 0));
+console.log(dividir(0, 7));
---
+++ dividir.js
@@ -0,0 +1,8 @@
+function dividir(num1, num2) {
+      if (num1 === 0 || num2 === 0) {
+            return "No se puede dividir por cero";
+      }
+      return num1 / num2;
+}
+
+module.exports = dividir;
